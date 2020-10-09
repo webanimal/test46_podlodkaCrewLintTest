c9831e91608f67d0225c126dcd74737acdda6e0f
Sergio <webanimal.group@gmail.com>
2020-10-09 14:37:12 +0300

    Initial commit. Created project with dependencies.

---
+++ app/src/main/java/ru/webanimal/test46podlodkacrewlinttest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.webanimal.test46podlodkacrewlinttest
+
+import android.os.Bundle
+import moxy.MvpAppCompatActivity
+
+class MainActivity : MvpAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
